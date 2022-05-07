caaf4c0822264352d54a6a6232d0e0cb0d6f0c2c
Kyjiya <sightquater@naver.com>
2022-05-07 11:32:06 +0900

    BLACK

--- main.py
+++ main.py
@@ -13,10 +13,11 @@ before_one_month = now - relativedelta(months=1)
 def save_file(data):
     folder = "data/"
     filename = f"FlareList_{now.strftime('%Y%m%d_%H%M%S')}.txt"
-    
-    if not os.path.exists(folder): os.makedirs(folder)
 
-    f = open(folder+filename, 'w', encoding="utf-8")
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    f = open(folder + filename, "w", encoding="utf-8")
     f.write(data)
     f.close()
 
@@ -38,7 +39,7 @@ def get_data():
 
     response = requests.get(
         url,
-        params = params,
+        params=params,
     )
 
     html = response.text
@@ -51,26 +52,33 @@ def get_data():
 
 def retail_data(data):
     retailed_data = ""
-    match = re.search(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}[.]\d{1}.+', data)
-    while match!=None:
-        match_data = data[match.regs[0][0]:match.regs[0][1]]
-        match_start = re.search(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', match_data)
-        match_start_datetime = datetime.strptime(match_data[match_start.regs[0][0]:match_start.regs[0][1]], "%Y-%m-%d %H:%M:%S")
-        match_end_data = match_data[match_start.regs[0][1]:]
-        match_end = re.search(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', match_end_data)
-        match_end_datetime = datetime.strptime(match_end_data[match_end.regs[0][0]:match_end.regs[0][1]], "%Y-%m-%d %H:%M:%S")
+    match = re.search(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}[.]\d{1}.+", data)
+
+    while match != None:
+        match_data = data[match.regs[0][0] : match.regs[0][1]]
+        match_start = re.search(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}", match_data)
+        match_start_datetime = datetime.strptime(
+            match_data[match_start.regs[0][0] : match_start.regs[0][1]],
+            "%Y-%m-%d %H:%M:%S",
+        )
+        match_end_data = match_data[match_start.regs[0][1] :]
+        match_end = re.search(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}", match_end_data)
+        match_end_datetime = datetime.strptime(
+            match_end_data[match_end.regs[0][0] : match_end.regs[0][1]],
+            "%Y-%m-%d %H:%M:%S",
+        )
 
         # DELETE the row outside specific time
         if before_one_month < match_start_datetime and match_end_datetime < now:
-            retailed_data = retailed_data + match_data + '\n'
-            
-        data = data[match.regs[0][1]:]
-        match = re.search(r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}[.]\d{1}.+', data)
+            retailed_data = retailed_data + match_data + "\n"
+
+        data = data[match.regs[0][1] :]
+        match = re.search(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}[.]\d{1}.+", data)
 
     return retailed_data
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
 
     data = get_data()
     data = retail_data(data)
